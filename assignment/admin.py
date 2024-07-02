a1b385731fdb19add587da542eb494ca7038ba46
k-sahil2307 <sahilkumarask5@gmail.com>
2024-07-02 15:46:05 +0530

    Initial Upload

---
+++ assignment/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Course,Assignment,Submission,TAs
+# Register your models here.
+admin.site.register(Course)
+admin.site.register(Assignment)
+admin.site.register(Submission)
+<<<<<<< HEAD
+admin.site.register(TAs)
+=======
+
+>>>>>>> d7925d5f30995df62dacef575b93f70d679c62e2
